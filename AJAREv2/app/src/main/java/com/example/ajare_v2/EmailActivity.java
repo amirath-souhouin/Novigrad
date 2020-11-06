499498b20d6216fd738415408c59dc62e778767f
BearWulF <elias.sidi@gmail.com>
2020-11-06 14:12:34 -0500

    Create EmailActivity.java

---
+++ AJAREv2/app/src/main/java/com/example/ajare_v2/EmailActivity.java
@@ -0,0 +1,93 @@
+package com.example.ajare_v2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class EmailActivity extends AppCompatActivity{
+
+    private static final String TAG = "EmailPassword";
+
+    EditText emailAddress, passwordd;
+    Button login;
+    TextView signUp;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email);
+        auth = FirebaseAuth.getInstance();
+        emailAddress = findViewById(R.id.emailAddress);
+        passwordd = findViewById(R.id.passworddd);
+        signUp = findViewById(R.id.create_btn);
+        login = findViewById(R.id.loginnn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // get user info
+                String email = emailAddress.getText().toString();
+                String smot = passwordd.getText().toString();
+
+                // validate
+                if(email.isEmpty()){
+                    emailAddress.setError("Please enter a valid email address.");
+                    emailAddress.requestFocus();
+                } else if (smot.isEmpty()){
+                    passwordd.setError("Please enter a password.");
+                    passwordd.requestFocus();
+                } else if(smot.isEmpty() && email.isEmpty()){
+                    Toast.makeText(EmailActivity.this, "Fields are empty!!", Toast.LENGTH_SHORT).show();
+                } else if(!(smot.isEmpty() && email.isEmpty())){
+
+                    // add username
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if(user.getDisplayName().equals(null)){
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(user.getEmail())
+                                .build();
+                        user.updateProfile(profileUpdates);
+                    }
+
+                    // once validated, login
+                    auth.signInWithEmailAndPassword(email, smot)
+                            .addOnCompleteListener(EmailActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(EmailActivity.this, "An error occurred...", Toast.LENGTH_SHORT).show();
+                            } else{
+                                startActivity(new Intent(EmailActivity.this, HomeActivity.class));
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(EmailActivity.this, "Oh no! An error has occurred...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EmailActivity.this, CreateActivity.class));
+            }
+        });
+    }
+}
